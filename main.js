f1f4865e3ed1d2e520a7cf1b068c4ca9d35d0dfb
yiguo-17 <66206441+yiguo-17@users.noreply.github.com>
2020-07-30 14:45:43 -0400

    add functions based on test file.

--- main.js
+++ main.js
@@ -1,15 +1,116 @@
 /********************
  * HELPER FUNCTIONS *
  ********************/
+const makeDino = function(name,time,meat,live){
+  const newOne = {
+    species:name,
+    period:time,
+    carnivore:meat,
+    extinct:live,
+  };
+  if(!live){newOne.extinct = false}
+  return newOne
+}
 
+const makeSingular = function(dinos){
+  const dino ={
+    species:dinos.species,
+    period:dinos.period,
+    carnivore:dinos.carnivore,
+    extinct:dinos.extinct,
+  };
+  if(dinos.species.slice(-2)==='us'){dino.species = dinos.species.slice(0,-2)}
+  return dino
+}
 
+const truncateSpecies = function(longDino){
+  const dino = {
+    species:longDino.species,
+    period:longDino.period,
+    carnivore:longDino.carnivore,
+    extinct:longDino.extinct,
+  }
+  if(longDino.species.length>=10){dino.species = longDino.species.slice(0,7)+'...'}
+  return dino
+}
+
+const makeExtinct = function(dino){
+  return{
+    species:dino.species,
+    period:dino.period,
+    carnivore:dino.carnivore,
+    extinct:true
+  }
+}
+
+const isCarnivore = function(dino){
+  return dino.carnivore;
+}
+const grass = function(dino){
+  return !dino.carnivore;
+}
+const isExtinct = function(dino){
+  return dino.extinct;
+}
+
+const keepBreed = function(dino){
+  return !dino.extinct
+}
+
+const isTriassic = function(dino){
+  return dino.period === 'Triassic';
+}
+
+const nextEra = function(dino){
+  return dino.period !== 'Triassic';
+}
+
+const isJurassic = function(dino){
+  return dino.period === 'Jurassic';
+}
+
+const isCretaceous = function(dino){
+  return dino.period === 'Cretaceous';
+}
 
 /***********************
  * ITERATION FUNCTIONS *
  **********************/
+const singularizeDinos = function(dinos){
+  return dinos.map(makeSingular)
+}
 
+const truncateDinos = function(dinos){
+  return dinos.map(truncateSpecies)
+}
 
+const makeAllExtinct = function(dinos){
+  return dinos.map(makeExtinct)
+}
 
+const carnivoresOnly = function(dinos){
+  return dinos.filter(isCarnivore)
+}
+
+const herbivoresOnly = function(dinos){
+  return dinos.filter(grass)
+}
+
+const extinctOnly = function(dinos){
+  return dinos.filter(isExtinct)
+}
+
+const notExtinct = function(dinos){
+  return dinos.filter(keepBreed)
+}
+
+const triassicOnly = function(dinos){
+  return dinos.filter(isTriassic)
+}
+
+const notTriassic = function(dinos){
+  return dinos.filter(nextEra)
+}
 /*********************************
  * TEST SETUP CODE - DON'T TOUCH!*
  ********************************/
